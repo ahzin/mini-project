81f733475e28cfe2aab8807eef53088e2ce15f90
AHZIN <ahzin0605@gmail.com>
2020-09-08 17:48:01 +0900

    .

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -4,11 +4,12 @@
 package mini.project;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+  public String getGreeting() {
+    return "Hello world.";
+  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+  public static void main(String[] args) {
+    System.out.println(new App().getGreeting());
+    System.out.println("조아진 ㅇ.<");
+  }
 }
