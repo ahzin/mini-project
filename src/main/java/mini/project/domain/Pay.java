305c2a966340125c0028a9e63c656aa8577bf74e
jaehyunYi <rmrmf@naver.com>
2020-09-09 19:39:37 +0900

    -

--- src/main/java/mini/project/domain/Pay.java
+++ src/main/java/mini/project/domain/Pay.java
@@ -1,5 +1,46 @@
 package mini.project.domain;
 
+import java.sql.Date;
+
 public class Pay {
 
+   private int no;
+   private int num;
+   private int method;
+   private Date deliveredDate;
+   private int viewCount;
+   public int getNo() {
+      return no;
+   }
+   public void setNo(int no) {
+      this.no = no;
+   }
+   public int getNum() {
+      return num;
+   }
+   public void setNum(int num) {
+      this.num = num;
+   }
+   
+   public Date getDeliveredDate() {
+      return deliveredDate;
+   }
+   public void setDeliveredDate(Date deliveredDate) {
+      this.deliveredDate = deliveredDate;
+   }
+   
+   public int getViewCount() {
+      return viewCount;
+   }
+   public void setViewCount(int viewCount) {
+      this.viewCount = viewCount;
+   }
+   public int getMethod() {
+      return method;
+   }
+   public void setMethod(int method) {
+      this.method = method;
+   }
+   
+   
 }
