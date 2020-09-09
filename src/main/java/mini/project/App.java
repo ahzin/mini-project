afbf1a73fc993d3418deae05c9ba5fe7e665c0dc
jaehyunYi <rmrmf@naver.com>
2020-09-09 09:55:11 +0900

    테스트

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -9,7 +9,6 @@ public class App {
   }
 
   public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
-    System.out.println("조아진 ㅇ.<");
+    System.out.println("오호라!!!!!!");
   }
 }
