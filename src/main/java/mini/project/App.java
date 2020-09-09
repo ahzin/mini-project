d8e464604a9a4826ec22fbec609592ef3f3bcefa
user <ahzin0605@gmail.com>
2020-09-09 10:10:17 +0900

    테스

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -5,7 +5,7 @@ package mini.project;
 
 public class App {
   public String getGreeting() {
-    return "Hello world.";
+    return "Hello world. Hello....H..e..ll...";
   }
 
   public static void main(String[] args) {
