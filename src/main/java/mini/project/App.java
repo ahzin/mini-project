ef05f2da39b61719ddeac0ba0e41dc42b70a77ea
user <ahzin0605@gmail.com>
2020-09-09 17:03:46 +0900

    클래스명 변경 및 App 소스 가져옴

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -3,21 +3,51 @@
  */
 package mini.project;
 
+import mini.project.util.Prompt;
+
 public class App {
-  public String getGreeting() {
-    return "HI HI";
-  }
-
   public static void main(String[] args) {
-    int a = 100;
-    if(a >50) {
-      System.out.println("크다!");
-    }
 
-    System.out.println("오호라!!!!!!");
-    System.out.println("졸리고 배고프네요....따흑쒸");
-    System.out.println("유부초밥");
-    System.out.println("맛있는거 드시네요...?저는 덕복희");
+    BoardHandler boardHandler = new BoardHandler();
+    MemberHandler memberHandler = new MemberHandler();
+    ProjectHandler projectHandler = new ProjectHandler(memberHandler);
+    TaskHandler taskHandler = new TaskHandler(memberHandler);
 
+    loop:
+      while (true) {
+        String command = Prompt.inputString("명령> ");
+
+        switch (command) {
+          case "/member/add": memberHandler.add(); break;
+          case "/member/list": memberHandler.list(); break;
+          case "/member/detail": memberHandler.detail(); break;
+          case "/member/update": memberHandler.update(); break;
+          case "/member/delete": memberHandler.delete(); break;
+          case "/project/add": projectHandler.add(); break;
+          case "/project/list": projectHandler.list(); break;
+          case "/project/detail": projectHandler.detail(); break;
+          case "/project/update": projectHandler.update(); break;
+          case "/project/delete": projectHandler.delete(); break;
+          case "/task/add": taskHandler.add(); break;
+          case "/task/list": taskHandler.list(); break;
+          case "/task/detail": taskHandler.detail(); break;
+          case "/task/update": taskHandler.update(); break;
+          case "/task/delete": taskHandler.delete(); break;
+          case "/board/add": boardHandler.add(); break;
+          case "/board/list": boardHandler.list(); break;
+          case "/board/detail": boardHandler.detail(); break;
+          case "/board/update": boardHandler.update(); break;
+          case "/board/delete": boardHandler.delete(); break;
+          case "quit":
+          case "exit":
+            System.out.println("안녕!");
+            break loop;
+          default:
+            System.out.println("실행할 수 없는 명령입니다.");
+        }
+        System.out.println(); // 이전 명령의 실행을 구분하기 위해 빈 줄 출력
+      }
+
+    Prompt.close();
   }
 }
